cde7c54493ef8a4ec17f839122c16ffcee859ec6
melansons <samuelmelanson@live.com>
2026-02-25 20:42:20 -0500

    success slider :fish:

--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 
 #define HEIGHT 600
 #define WIDTH 900
-#define SLIDDER_HEIGHT 400
-#define SLIDDER_WIDTH 20
-#define SLIDDER_X 100
-#define SLIDDER_Y 100
-#define REEL_UPPERBOUND SLIDDER_Y
-#define REEL_LOWERBOUND SLIDDER_Y + SLIDDER_HEIGHT
+#define FISH_SLIDER_HEIGHT 400
+#define FISH_SLIDER_WIDTH 20
+#define FISH_SLIDER_X 100
+#define FISH_SLIDER_Y 100
+#define REEL_UPPERBOUND FISH_SLIDER_Y
+#define REEL_LOWERBOUND FISH_SLIDER_Y + FISH_SLIDER_HEIGHT
+#define SUCCESS_SLIDER_X 150
+#define SUCCESS_SLIDER_Y 500
+#define SUCCESS_SLIDER_HEIGHT 40
+#define SUCCESS_SLIDER_WIDTH 600
 
 typedef enum {
     UP = -1,
@@ -30,7 +34,16 @@ typedef struct {
     dir_t direction;
     int random_number;
     int change_dir_chance;
+    Vector2 pos;
+    Texture2D texture;
 } fish_t;
+
+typedef struct {
+    Rectangle rec;
+    float completionVal;
+    Rectangle completionRec;
+    Color color;
+} success_slider_t;
 
 void moveReel(short direction, reel_t* reel);
 void moveFish(fish_t* fish);
@@ -46,31 +59,42 @@ int main(void) {
 
     SetTargetFPS(60);
 
-    Rectangle slidder = {SLIDDER_X, SLIDDER_Y, SLIDDER_WIDTH, SLIDDER_HEIGHT};
-    reel_t reel = {{SLIDDER_X, 100, SLIDDER_WIDTH, 20}, 1, WHITE};
-    fish_t fish = {{SLIDDER_X, 100, SLIDDER_WIDTH, 50}, 1, GREEN, DOWN, 1, 8};
+    Rectangle fish_slider = {FISH_SLIDER_X, FISH_SLIDER_Y, FISH_SLIDER_WIDTH, FISH_SLIDER_HEIGHT};
+    success_slider_t success_slider = {{SUCCESS_SLIDER_X, SUCCESS_SLIDER_Y, SUCCESS_SLIDER_WIDTH, SUCCESS_SLIDER_HEIGHT}, 0.5, {SUCCESS_SLIDER_X, SUCCESS_SLIDER_Y, SUCCESS_SLIDER_WIDTH/2, SUCCESS_SLIDER_HEIGHT}, YELLOW};
+    reel_t reel = {{FISH_SLIDER_X, 100, FISH_SLIDER_WIDTH, 10}, 1, WHITE};
+    fish_t fish = {{FISH_SLIDER_X, 100, FISH_SLIDER_WIDTH, 50}, 1, GREEN, DOWN, 1, 8,{300, 300}, fish_texture};
     dir_t direction = UP;
-    Vector2 fish_pos = {300, 300};
+    bool reelOnFish = false;
 
     while(!WindowShouldClose())
     {
+        reelOnFish = checkRecCollision(&reel.rec, &fish.rec);
         if(IsKeyDown(KEY_SPACE)) {
             direction = UP;
         } else {
             direction = DOWN;
         }
+        if(reelOnFish && success_slider.completionVal > 0 && success_slider.completionVal < 1){
+            success_slider.completionVal += 0.003;
+        } else {
+            success_slider.completionVal -= 0.002;
+        }
+        success_slider.completionRec.width = SUCCESS_SLIDER_WIDTH * success_slider.completionVal;
+
         BeginDrawing();
             ClearBackground(SKYBLUE);
             moveReel(direction, &reel);
             moveFish(&fish);
             DrawRectangleRec(fish.rec, fish.color);
             DrawRectangleRec(reel.rec, reel.color);
-            DrawTextureEx(fish_texture, fish_pos, 1, 0.3, WHITE);
-            if(checkRecCollision(&reel.rec, &fish.rec)){
+            DrawTextureEx(fish.texture, fish.pos, 1, 0.3, WHITE);
+
+            if(reelOnFish){
                 DrawText("Collision detected", 50, 50, 20, DARKGRAY);
             }
-
-            DrawRectangleLinesEx(slidder, 2, BLACK);
+            DrawRectangleLinesEx(fish_slider, 2, BLACK);
+            DrawRectangleRec(success_slider.completionRec, success_slider.color);
+            DrawRectangleLinesEx(success_slider.rec, 2, BLACK);
         EndDrawing();
     }
 
